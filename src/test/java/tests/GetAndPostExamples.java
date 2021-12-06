a423521d67ebea46a6bfac832e5affc4f647eb4d
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-06 10:11:40 -0300

    Test: add basic implementation for getTest()

--- src/test/java/tests/GetAndPostExamples.java
+++ src/test/java/tests/GetAndPostExamples.java
@@ -1,2 +1,18 @@
-package tests;public class GetAndPostExamples {
+package tests;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+@SuppressWarnings("NewClassNamingConvention")
+public class GetAndPostExamples {
+
+    @Test
+    public void getTest() {
+        baseURI = "https://reqres.in/api";
+
+        given().get("/users?page=1")
+                .then().statusCode(200);
+    }
+
 }
