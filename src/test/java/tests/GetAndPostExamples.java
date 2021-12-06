9b8818364672cc81664415bc3e8ea71bcd74d518
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-06 11:48:41 -0300

    Test: create a postTest successfully passing a json request body

--- src/test/java/tests/GetAndPostExamples.java
+++ src/test/java/tests/GetAndPostExamples.java
@@ -1,8 +1,11 @@
 package tests;
 
+import io.restassured.http.ContentType;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItems;
 
@@ -18,6 +21,24 @@ public class GetAndPostExamples {
                 .statusCode(200)
                 .body("data[4].first_name", equalTo("Charles"))
                 .body("data.first_name", hasItems("George", "Janet"))
+        ;
+    }
+
+    @Test
+    public void postTest() {
+        JSONObject request = new JSONObject();
+        request.put("name", "Andrew");
+        request.put("job", "Automation Tester");
+
+        baseURI = "https://reqres.in/api";
+
+        given().body(request.toString())
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .when()
+                .post("/users")
+                .then()
+                .statusCode(201)
+                .log().all()
         ;
     }
 
