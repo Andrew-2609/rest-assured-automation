4b476f0af019712965c64a247cb0d6ab29328066
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-06 10:16:15 -0300

    Test: add assertions to body response in secondTest()

--- src/test/java/tests/GetAndPostExamples.java
+++ src/test/java/tests/GetAndPostExamples.java
@@ -3,6 +3,8 @@ package tests;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
 
 @SuppressWarnings("NewClassNamingConvention")
 public class GetAndPostExamples {
@@ -12,7 +14,11 @@ public class GetAndPostExamples {
         baseURI = "https://reqres.in/api";
 
         given().get("/users?page=1")
-                .then().statusCode(200);
+                .then()
+                .statusCode(200)
+                .body("data[4].first_name", equalTo("Charles"))
+                .body("data.first_name", hasItems("George", "Janet"))
+        ;
     }
 
 }
